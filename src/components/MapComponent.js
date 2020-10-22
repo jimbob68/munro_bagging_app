49d051f2865946d68e62c65c38f70932feaa2832
James Fraser <jimbob4t@virginmedia.com>
2020-10-22 14:29:29 +0100

    try to fix icon when zooming in and out

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
 import ApiKey from '../ApiKey.js';
 import './MapComponent.css'
 import  '../../node_modules/@tomtom-international/web-sdk-maps/dist/maps.css';
 
 const MapComponent = () => {
+
+    const [ zoomLevel, setZoomLevel ] = useState(5)
 
     useEffect(() => {
         const map = tt.map({
@@ -17,15 +19,34 @@ const MapComponent = () => {
     });
     map.addControl(new tt.FullscreenControl());
     map.addControl(new tt.NavigationControl());
-    addMarkers(map);
+    addMarkers(map, 0.5);
+    console.log("zoom", map.getZoom())
+    map.on("zoomend", () => zoomFunction(map))
     }, []) 
 
-    const addMarkers = (map) => {
-        const marker = new tt.Marker({scale: 0.5})
+    const addMarkers = (map, size) => {
+        let marker = 0;
+        if(marker){marker.remove()}
+            marker = new tt.Marker({scale: size})
             .setLngLat([-5.003525, 56.796849])
             .addTo(map)
+
+        console.log("thing", marker.getElement())
+        const x = marker.getElement()
+        x.style.backgroundColor = "rgba(51, 170, 51)"
+        // x.style.opacity = "0"
+        x.style.height = "inherit"
+
+        console.log("element", x.parentElement)
+        const y = x.parentElement
+        y.style.backgroundColor = "red"
     }
 
+    const zoomFunction = (map) => {
+        console.log("zoomedout")
+        setZoomLevel(map.getZoom())
+        addMarkers(map, (map.getZoom() / 10))
+    }
     
     
     return (
