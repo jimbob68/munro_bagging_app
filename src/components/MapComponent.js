ad76936c615aedbb2f5ceebfab7326474aa5192b
James Fraser <jimbob4t@virginmedia.com>
2020-10-22 12:38:26 +0100

    markers on map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
 import ApiKey from '../ApiKey.js';
 import './MapComponent.css'
-// import  '../../node_modules/@tomtom-international/web-sdk-maps/dist/maps.css';
+import  '../../node_modules/@tomtom-international/web-sdk-maps/dist/maps.css';
 
 const MapComponent = () => {
 
@@ -12,19 +12,26 @@ const MapComponent = () => {
         container: 'map',
         center: [-4.0000, 56.0000],
         zoom: 5,
-        style: 'tomtom://vector/1/basic-main'
-        // dragPan: !isMobileOrTablet()
+        style: 'tomtom://vector/1/basic-main',
+        dragPan: true
     });
     map.addControl(new tt.FullscreenControl());
     map.addControl(new tt.NavigationControl());
+    addMarkers(map);
     }, []) 
+
+    const addMarkers = (map) => {
+        const marker = new tt.Marker({scale: 0.5})
+            .setLngLat([-5.003525, 56.796849])
+            .addTo(map)
+    }
 
     
     
     return (
         <>
             <div class="container">
-            <div id="map"></div>
+                <div id="map"></div>
             </div>
     
     
