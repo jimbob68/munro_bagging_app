325227256cb4538b51aed1dfe384d086379f07bb
James Fraser <jimbob4t@virginmedia.com>
2020-10-28 13:07:31 +0000

    Added firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Main from './Main.js';
+import RegisterPage from './RegisterPage.js';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello World!</h1>
+			<RegisterPage />
 			<Main />
 		</div>
 	);
