1e4cfc643e139a6e773d2c1336734f07bf17f4aa
James Fraser <jimbob4t@virginmedia.com>
2020-10-29 12:34:28 +0000

    Data persisting in database including undo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Main from './Main.js';
 import RegisterPage from './RegisterPage.js';
+import firebase from 'firebase/app';
+import db from './firebaseConfig';
+import 'firebase/auth';
 
 function App() {
+	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
+	useEffect(() => {
+		checkIfLoggedIn();
+	}, []);
+
+	const checkIfLoggedIn = () => {
+		if (firebase.auth().currentUser) {
+			setIsLoggedIn(true);
+		}
+	};
+
 	return (
 		<div className="App">
 			<h1>Hello World!</h1>
-			<RegisterPage />
-			<Main />
+			{firebase.auth().currentUser ? <Main /> : <RegisterPage setIsLoggedIn={setIsLoggedIn} />}
+			{/* <RegisterPage />
+			<Main /> */}
 		</div>
 	);
 }
