f53d9112690ac972d2ee2e9f78f5e3f9311523d0
James Fraser <jimbob4t@virginmedia.com>
2020-11-03 14:19:31 +0000

    alerts added to login and register.

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -25,7 +25,7 @@ const RegisterPage = ({ setIsLoggedIn }) => {
 				setEmail('');
 				setPassword('');
 			})
-			.catch((error) => console.log(error));
+			.catch((error) => alert(error));
 	};
 
 	const handleLogin = () => {
@@ -37,7 +37,7 @@ const RegisterPage = ({ setIsLoggedIn }) => {
 				setLoginEmail('');
 				setLoginPassword('');
 			})
-			.catch((error) => console.log(error));
+			.catch((error) => alert(error));
 		// if(firebase.auth().currentUser){
 		//     setIsLoggedIn(true)
 		// }
