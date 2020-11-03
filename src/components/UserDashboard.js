afd91c3dc76663fc200a447b03bb916840e60ecc
James Fraser <jimbob4t@virginmedia.com>
2020-11-03 12:30:31 +0000

    Added height and name of highest munro climbed to dashboard.

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import db from '../firebaseConfig';
 import 'firebase/auth';
@@ -6,9 +6,14 @@ import './UserDashboard.css';
 
 const UserDashboard = ({setIsLoggedIn, climbedMunros, munroData}) => {
 
+    const [ highestMunroData, setHighestMunroData ] = useState({});
+
     useEffect(() => {
         completedMunros()
-    }, [climbedMunros])
+        if(munroData.length){
+            getHighestMunro()
+        }
+    }, [climbedMunros, munroData])
 
     const handleLogout = () => {
         firebase.auth().signOut()
@@ -24,11 +29,35 @@ const UserDashboard = ({setIsLoggedIn, climbedMunros, munroData}) => {
             alert("Congratulations you've climbed all of Scotland's Munros!")
         }
     }
+    const getHighestMunro = () => {
+        console.log("climbedMunros", climbedMunros)
+        console.log("munroData", munroData)
+        const munroIdsWithoutLetter = climbedMunros.map(munro => {
+         const munroIdWithoutLetter = munro.substring(1)
+         return parseInt(munroIdWithoutLetter)
+        })
+        console.log(munroIdsWithoutLetter)
+        const lowestConqueredMunroId = Math.min(...munroIdsWithoutLetter)
+        console.log(lowestConqueredMunroId)
+        const highestMunroConquered = munroData.find(munro => {
+            if(lowestConqueredMunroId < 100 && lowestConqueredMunroId >= 10){
+               return munro.smcid === "M0" + lowestConqueredMunroId 
+            }else if(lowestConqueredMunroId < 10){
+                return munro.smcid === "M00" + lowestConqueredMunroId
+            }else{
+                return munro.smcid === "M" + lowestConqueredMunroId 
 
+        }
+
+        
+    })
+        setHighestMunroData(highestMunroConquered)
+    }
     return (
         <>
         <p>User Dashboard</p>
-    <p>Munros Climbed: {climbedMunros.length} / 282</p>
+    <p>Munros Conquered: {climbedMunros.length} / 282</p>
+    { highestMunroData.name && <p>Highest Munro Conquered: {highestMunroData.name} {highestMunroData.height}m</p>}
         <button onClick={() => handleLogout()}>Logout</button>
     
     </>
